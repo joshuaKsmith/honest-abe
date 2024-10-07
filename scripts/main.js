0eed3fe0a331b8c3ceff64637b6eb8318371958b
unknown <mtgmirage@protonmail.com>
2024-10-07 15:42:54 -0500

    built pacs.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,26 @@
 import { GetPoliticians } from "./politicians.js"
 import { GetCorporations } from "./corporations.js"
+import { GetPacs } from "./pacs.js"
+
 
 const container = document.querySelector("#container")
 
 const render = async () => {
     const politicianListHTML = await GetPoliticians()
     const corporationListHTML = await GetCorporations()
+    const pacListHTML = await GetPacs()
 
     container.innerHTML = `
         <article class="politicians">
             ${politicianListHTML}
         </article>
+        ----------------------------------------------------------------------------------------------------
         <article class="corporations">
             ${corporationListHTML}
+        </article>
+        ----------------------------------------------------------------------------------------------------
+        <article class="pacs">
+            ${pacListHTML}
         </article>
     `
 }
---
+++ scripts/pacs.js
@@ -0,0 +1,34 @@
+
+
+export const GetPacs = async () => {
+    const responseA = await fetch("http://localhost:8088/pacs")
+    const pacs = await responseA.json()
+
+    const responseB = await fetch("http://localhost:8088/corporateDonations?_expand=corporation")
+    const corporateDonations = await responseB.json()
+
+
+    return pacs.map((pac) => {
+        const filteredDonations = corporateDonations.filter((corporateDonation) => 
+            pac.id === corporateDonation.pacId
+        )
+        return `<section class="pac">
+            <header class="pac__name">
+                <h1>${pac.registeredName}</h1>
+            </header>
+            <div class="pac__info">
+                <div>${pac.address}</div>
+            </div>
+            <div class="pac__donors">
+                <h2>Donors</h2>
+                <ul>
+                    ${
+                        filteredDonations.map((donation) => {
+                            return `<li>${donation.corporation.company} ($${donation.amount})</li>`
+                        }).join("")
+                    }
+                </ul>
+            </div>
+        </section>`
+    }).join("")
+}
